7ed756f154337897df84e048ff46bef5a2d6da49
madyanes <madyanes.dev@gmail.com>
2023-07-24 04:48:40 +0700

    add uncompleted todo list

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener(RENDER_EVENT, function () {
+    const uncompletedTODOList = document.getElementById('todos');
+    uncompletedTODOList.innerHTML = '';
+
+    for (const todoItem of todos) {
+        const todoElement = makeTodo(todoItem);
+        uncompletedTODOList.append(todoElement);
+    }
+});
+
 function addTodo() {
     const textTodo = document.getElementById('title').value;
     const timestamp = document.getElementById('date').value;
@@ -36,3 +46,22 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
 document.addEventListener(RENDER_EVENT, function () {
     console.log(todos);
 });
+
+function makeTodo(todoObject) {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    return container;
+}
