a983e7eff334e44f3cfd347de3c61c737b319e1e
madyanes <madyanes.dev@gmail.com>
2023-07-24 08:15:52 +0700

    render on load

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         addTodo();
     });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
 });
 
 document.addEventListener(RENDER_EVENT, function () {
@@ -171,3 +175,16 @@ function isStorageExist() /* boolean */ {
 document.addEventListener(SAVED_EVENT, function () {
     console.log(localStorage.getItem(STORAGE_KEY));
 });
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const todo of data) {
+            todos.push(todo);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
